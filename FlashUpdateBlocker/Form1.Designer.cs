5d8d827233c29bbc6d1e6dfccd0937d5e8d65b59
NickPikachu <NickPikachu@LENOVO-AIR14>
2023-09-08 14:47:59 +0800

    添加项目文件。

---
+++ FlashUpdateBlocker/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace FlashUpdateBlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FlashUpdateBlocker v1.0.1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(343, 45);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clicking the button below will modify your\r\nhosts.txt and Adobe Flash Player data" +
+    " to\r\nprevent update detection and prompts.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(322, 45);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Warning: Adobe Flash Player stopped\r\nupdating. Using old versions may\r\nincrease s" +
+    "ecurity risks.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(343, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(9, 210);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(311, 15);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "This project is open source on Github.";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 242);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FlashUpdateBlocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ FlashUpdateBlocker/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FlashUpdateBlocker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 添加 hosts 屏蔽 Flash 在线更新检查
+            string hostsFilePath = @"C:\Windows\System32\drivers\etc\hosts";
+            string contentToAdd = "127.0.0.1 geo2.adobe.com\r\n127.0.0.1 fpdownload2.macromedia.com\r\n127.0.0.1 fpdownload.macromedia.com\r\n127.0.0.1 macromedia.com";
+
+            try
+            {
+                using (StreamWriter sw = File.AppendText(hostsFilePath))
+                {
+                    sw.WriteLine(contentToAdd);
+                }
+
+                // 重载网络配置以应用 hosts 配置
+                RunCommand("ipconfig", "/flushdns");
+
+                DeleteAndCreateSettingsSol();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unknown error occurred: " + ex.Message);
+            }
+        }
+
+        private void RunCommand(string command, string arguments)
+        {
+            // 创建命令行执行命令
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = command;
+            processStartInfo.Arguments = arguments;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private void DeleteAndCreateSettingsSol()
+        {
+            // 删除并锁定 Flash 最新版本配置文件防止提示更新
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Macromedia\Flash Player\macromedia.com\support\flashplayer\sys");
+            string file1Path = Path.Combine(folderPath, "settings.sol");
+            string file2Path = Path.Combine(folderPath, "#local\\settings.sol");
+
+            try
+            {
+                if (File.Exists(file1Path))
+                {
+                    File.Delete(file1Path);
+                }
+
+                if (File.Exists(file2Path))
+                {
+                    File.Delete(file2Path);
+                }
+
+                using (FileStream fs = File.Create(file1Path))
+                {
+                    fs.Close();
+                }
+
+                File.SetAttributes(file1Path, FileAttributes.ReadOnly);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unknown error occurred: " + ex.Message);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // 觉得有用不妨 Star 一下
+            System.Diagnostics.Process.Start("https://github.com/NickPikachu/FlashUpdateBlocker");
+
+        }
+    }
+}
