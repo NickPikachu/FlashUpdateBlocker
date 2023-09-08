917315bea535a4dbd00b7472d470565c9a4e035e
nick66015@gmail.com <nick66015@gmail.com>
2023-09-08 16:32:08 +0800

    v1.0.2 Update

--- FlashUpdateBlocker/Form1.Designer.cs
+++ FlashUpdateBlocker/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace FlashUpdateBlocker
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -44,7 +45,7 @@ namespace FlashUpdateBlocker
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(207, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "FlashUpdateBlocker v1.0.1";
+            this.label1.Text = "FlashUpdateBlocker v1.0.2";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -62,7 +63,7 @@ namespace FlashUpdateBlocker
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Location = new System.Drawing.Point(9, 198);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(322, 45);
             this.label3.TabIndex = 2;
@@ -75,14 +76,14 @@ namespace FlashUpdateBlocker
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(343, 40);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Start";
+            this.button1.Text = "Block update prompts";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(9, 210);
+            this.linkLabel1.Location = new System.Drawing.Point(9, 254);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(311, 15);
             this.linkLabel1.TabIndex = 4;
@@ -90,11 +91,22 @@ namespace FlashUpdateBlocker
             this.linkLabel1.Text = "This project is open source on Github.";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(343, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Block FF News";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 242);
+            this.ClientSize = new System.Drawing.Size(370, 285);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -116,6 +128,7 @@ namespace FlashUpdateBlocker
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- FlashUpdateBlocker/Form1.cs
+++ FlashUpdateBlocker/Form1.cs
@@ -19,35 +19,55 @@ namespace FlashUpdateBlocker
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // 添加 hosts 屏蔽 Flash 在线更新检查
+            DialogResult result = MessageBox.Show("Are you sure you want to block the Flash live update check?", "Second confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                // 添加 hosts 屏蔽 Flash 在线更新检查
+                string hostsFilePath = @"C:\Windows\System32\drivers\etc\hosts";
+                string contentToAdd = "127.0.0.1 geo2.adobe.com\r\n127.0.0.1 fpdownload2.macromedia.com\r\n127.0.0.1 fpdownload.macromedia.com\r\n127.0.0.1 macromedia.com";
+
+                    using (StreamWriter sw = File.AppendText(hostsFilePath))
+                    {
+                        sw.WriteLine(contentToAdd);
+                    }
+
+                    // 重载网络配置以应用 hosts 配置
+                    RunCommand("ipconfig", "/flushdns");
+
+                    DeleteSettingsSol();
+
+                    MessageBox.Show("Blocking update successful!");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 添加 hosts 屏蔽 FF新推荐 数据获取途径
             string hostsFilePath = @"C:\Windows\System32\drivers\etc\hosts";
-            string contentToAdd = "127.0.0.1 geo2.adobe.com\r\n127.0.0.1 fpdownload2.macromedia.com\r\n127.0.0.1 fpdownload.macromedia.com\r\n127.0.0.1 macromedia.com";
+            string contentToAdd = "0.0.0.0 mini.ffnews.cn\r\n0.0.0.0 tongji.flash.cn\r\n0.0.0.0 mini.flash.2144.com";
 
-            try
+            using (StreamWriter sw = File.AppendText(hostsFilePath))
             {
-                using (StreamWriter sw = File.AppendText(hostsFilePath))
-                {
-                    sw.WriteLine(contentToAdd);
-                }
-
-                // 重载网络配置以应用 hosts 配置
-                RunCommand("ipconfig", "/flushdns");
-
-                DeleteAndCreateSettingsSol();
+                sw.WriteLine(contentToAdd);
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An unknown error occurred: " + ex.Message);
-            }
+
+            // 
+
+            RunCommand("sc", "config \"Flash Helper Service\" start= disabled");
+
+            MessageBox.Show("Blocking FF News successful!");
         }
 
         private void RunCommand(string command, string arguments)
         {
             // 创建命令行执行命令
-            ProcessStartInfo processStartInfo = new ProcessStartInfo();
-            processStartInfo.FileName = command;
-            processStartInfo.Arguments = arguments;
-            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = command,
+                Arguments = arguments,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
 
             using (Process process = new Process())
             {
@@ -57,37 +77,56 @@ namespace FlashUpdateBlocker
             }
         }
 
-        private void DeleteAndCreateSettingsSol()
+        private void DeleteSettingsSol()
         {
             // 删除并锁定 Flash 最新版本配置文件防止提示更新
             string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Macromedia\Flash Player\macromedia.com\support\flashplayer\sys");
             string file1Path = Path.Combine(folderPath, "settings.sol");
             string file2Path = Path.Combine(folderPath, "#local\\settings.sol");
-
-            try
+            string file3Path = Path.Combine(folderPath, "settings.sxx");
+            if (File.Exists(file1Path))
             {
-                if (File.Exists(file1Path))
+                bool file1ReadOnly = (File.GetAttributes(file1Path) & FileAttributes.ReadOnly) == FileAttributes.ReadOnly;
+                if (!file1ReadOnly || (file1ReadOnly && new FileInfo(file1Path).Length > 0))
                 {
                     File.Delete(file1Path);
+                    using (FileStream fs = File.Create(file1Path))
+                    {
+                        fs.Close();
+                    }
+                    File.SetAttributes(file1Path, FileAttributes.ReadOnly);
                 }
+            }
 
-                if (File.Exists(file2Path))
+            if (File.Exists(file2Path))
+            {
+                bool file2ReadOnly = (File.GetAttributes(file2Path) & FileAttributes.ReadOnly) == FileAttributes.ReadOnly;
+                if (!file2ReadOnly || (file2ReadOnly && new FileInfo(file2Path).Length > 0))
                 {
                     File.Delete(file2Path);
+                    using (FileStream fs = File.Create(file2Path))
+                    {
+                        fs.Close();
+                    }
+                    File.SetAttributes(file2Path, FileAttributes.ReadOnly);
                 }
-
-                using (FileStream fs = File.Create(file1Path))
-                {
-                    fs.Close();
-                }
-
-                File.SetAttributes(file1Path, FileAttributes.ReadOnly);
             }
-            catch (Exception ex)
+
+            if (File.Exists(file3Path))
             {
-                MessageBox.Show("An unknown error occurred: " + ex.Message);
+                bool file3ReadOnly = (File.GetAttributes(file3Path) & FileAttributes.ReadOnly) == FileAttributes.ReadOnly;
+                if (!file3ReadOnly || (file3ReadOnly && new FileInfo(file3Path).Length > 0))
+                {
+                    File.Delete(file3Path);
+                    using (FileStream fs = File.Create(file3Path))
+                    {
+                        fs.Close();
+                    }
+                    File.SetAttributes(file3Path, FileAttributes.ReadOnly);
+                }
             }
         }
+
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
